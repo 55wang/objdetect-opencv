942bc0181d688c06a67a6e21b30198d5852bbf32
Wang Zhe <wang@Wangs-MacBook-Pro.local>
2014-01-29 02:17:33 +0800

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Wang Zhe on 29/1/14.
+//  Copyright (c) 2014 Wang Zhe. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
